99d58be7c17c9519795b8766025dbf8ba31737f5
Michael Staib <michael@chillicream.com>
2025-09-02 23:23:17 +0200

    Only export schema on dev

--- src/Defaults/SourceSchemaDefaults/Extensions.cs
+++ src/Defaults/SourceSchemaDefaults/Extensions.cs
@@ -5,17 +5,26 @@ namespace Microsoft.Extensions.Hosting;
 
 public static class Extensions
 {
+    private const string Production = nameof(Production); 
+
     public static IRequestExecutorBuilder AddDefaultSettings(
         this IRequestExecutorBuilder builder,
         bool registerNodeInterface = true)
     {
+        var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
         builder.AddGlobalObjectIdentification(registerNodeInterface);
         builder.AddInstrumentation();
         builder.AddMutationConventions();
         builder.AddPagingArguments();
         builder.AddQueryContext();
         builder.ModifyCostOptions(x => x.EnforceCostLimits = false);
-        builder.ExportSchemaOnStartup();
+
+        if (Production.Equals(environmentName, StringComparison.OrdinalIgnoreCase))
+        {
+            builder.ExportSchemaOnStartup();
+        }
+        
         return builder;
     }
 }
