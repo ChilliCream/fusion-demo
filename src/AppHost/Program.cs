3d5ffb0b84b9753513f458260a01f5acb6056841
Glen <glen.84@gmail.com>
2025-12-05 09:19:36 +0200

    Fixed issues

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -11,47 +11,47 @@ var keycloak = builder
 var accountsApi = builder
     .AddProject<Projects.Demo_Accounts>("accounts-api")
     .WithReference(postgres.AddDatabase("accounts-db"))
-    .WithGraphQLSchemaEndpoint(endpointName: "accounts")
+    .WithGraphQLSchemaEndpoint()
     .WaitFor(postgres);
 
 var inventoryApi = builder
     .AddProject<Projects.Demo_Inventory>("inventory-api")
     .WithReference(postgres.AddDatabase("inventory-db"))
-    .WithGraphQLSchemaEndpoint(endpointName: "inventory")
+    .WithGraphQLSchemaEndpoint()
     .WaitFor(postgres);
 
 var orderApi = builder
     .AddProject<Projects.Demo_Order>("order-api")
     .WithReference(postgres.AddDatabase("order-db"))
-    .WithGraphQLSchemaEndpoint(endpointName: "order")
+    .WithGraphQLSchemaEndpoint()
     .WaitFor(postgres);
 
 var paymentsApi = builder
     .AddProject<Projects.Demo_Payments>("payments-api")
     .WithReference(postgres.AddDatabase("payments-db"))
-    .WithGraphQLSchemaEndpoint(endpointName: "payments")
+    .WithGraphQLSchemaEndpoint()
     .WaitFor(postgres);
 
 var productsApi = builder
     .AddProject<Projects.Demo_Products>("products-api")
     .WithReference(postgres.AddDatabase("products-db"))
-    .WithGraphQLSchemaEndpoint(endpointName: "products")
+    .WithGraphQLSchemaEndpoint()
     .WaitFor(postgres);
 
 var reviewsApi = builder
     .AddProject<Projects.Demo_Reviews>("reviews-api")
     .WithReference(postgres.AddDatabase("reviews-db"))
-    .WithGraphQLSchemaEndpoint(endpointName: "reviews")
+    .WithGraphQLSchemaEndpoint()
     .WaitFor(postgres);
 
 var shippingApi = builder
     .AddProject<Projects.Demo_Shipping>("shipping-api")
-    .WithGraphQLSchemaEndpoint(endpointName: "shipping");
+    .WithGraphQLSchemaEndpoint();
 
 var cartApi = builder
     .AddProject<Projects.Demo_Cart>("cart-api")
     .WithReference(postgres.AddDatabase("cart-db"))
-    .WithGraphQLSchemaEndpoint(endpointName: "cart")
+    .WithGraphQLSchemaEndpoint()
     .WaitFor(postgres);
 
 builder
