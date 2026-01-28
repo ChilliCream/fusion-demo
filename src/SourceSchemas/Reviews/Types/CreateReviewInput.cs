025b3042a7372a69add6df06d9dc35a12dbf6a82
Michael Staib <michael@chillicream.com>
2026-01-28 08:22:06 +0000

    Demo 2 Starting Point

--- src/SourceSchemas/Reviews/Types/CreateReviewInput.cs
+++ src/SourceSchemas/Reviews/Types/CreateReviewInput.cs
@@ -3,5 +3,6 @@ namespace Demo.Reviews.Types;
 public record CreateReviewInput(
     string Body, 
     int Stars, 
-    [property: ID<Product>] int ProductId, 
+    int ProductId,
+    // [property: ID<Product>] int ProductId, 
     [property: ID<User>] int AuthorId);
